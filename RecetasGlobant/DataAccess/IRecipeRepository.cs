62e0577e39e466db2c0f46b5b51d470816a91451
Ivan Egorov <ivaneg_23@hotmail.com>
2020-04-23 15:01:37 -0300

    Changes

--- RecetasGlobant/DataAccess/IRecipeRepository.cs
+++ RecetasGlobant/DataAccess/IRecipeRepository.cs
@@ -19,9 +19,11 @@ namespace DataAccess
 
         public abstract void AddRecipe(Recipe recipe);
 
-        public abstract void DeleteRecipe(Recipe recipe);
+        public abstract void DeleteRecipe(int id);
 
         public abstract List<Ingredient> GetAllIngredients(Ingredient ingredient);
+
+        public abstract Recipe GetRecipe(int Id);
 
     }
 }
