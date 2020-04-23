d00afe06279247ff536ca1e288f43fbebdd56a61
Giannina Benzo <gianninabenzo@hotmail.com>
2020-04-23 14:32:31 -0300

    Cambio Gianni, domain y context

--- RecetasGlobant/DataAccess/RecipesContext.cs
+++ RecetasGlobant/DataAccess/RecipesContext.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 //Gianni
 namespace DataAccess
 {
-    class RecipesContext
+    public class RecipesContext : DbContext
     {
+        public RecipesContext()
+        {
+
+        }
+
+        public DbSet<Recipe> Recipes { get; set; }
+            
+        public DbSet<Ingredient> Ingredients { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RecipeIngredient>()
+                .HasKey(bc => new { bc.RecipeID, bc.IngredientID });
+            modelBuilder.Entity<RecipeIngredient>()
+                .HasOne(bc => bc.Recipe)
+                .WithMany(b => b.RecipeIngredients)
+                .HasForeignKey(bc => bc.IngredientID);
+            modelBuilder.Entity<RecipeIngredient>()
+                .HasOne(bc => bc.Ingredient)
+                .WithMany(c => c.RecipeIngredients)
+                .HasForeignKey(bc => bc.RecipeID);
+        }
 
     }
 }
